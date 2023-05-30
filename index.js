bccf9f64e8b0df9c125a2564cd3c3a5c0eea2e58
FacundoFigueroa23 <facundoleonelfigueroa@hotmail.com>
2023-05-30 11:54:51 -0300

    Proyecto funcionando

---
+++ index.js
@@ -0,0 +1,16 @@
+const getDeficit = (sexo, peso, altura, edad, actividad) => {
+    if (sexo === "hombre") {
+        return (10*peso + 6.25*altura - 5*edad + 5) * actividad
+    } else {
+        return (10*peso + 6.25*altura - 5*edad - 161) * actividad
+    }
+}
+
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault()
+    const data = Object.fromEntries(
+        new FormData(e.target)
+    )
+    let deficit = getDeficit(data.sexo, Number(data.peso), Number(data.altura), Number(data.edad), Number(data.actividad))
+    alert(`Tu déficit calórico es de ${deficit - 500} a ${deficit - 300} calorías diarias.`)
+})
